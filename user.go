fc3f2f3263fd2a0eb0546878da4a8fb85aee3afd
Ewokkkkk <ewok.mfk@gmail.com>
2020-09-09 18:54:06 +0900

    add: ユーザー作成機能
    json形式のnameを取得し、jwtを付与し、DB挿入

--- user.go
+++ user.go
@@ -1,24 +1,30 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"strconv"
+
+	_ "github.com/go-sql-driver/mysql"
+
+	"github.com/dgrijalva/jwt-go"
 )
 
 type User struct {
-	Name string `json:"name"`
+	Name  string `json:"name"`
+	Token string
 }
 
-func createUser(w http.ResponseWriter, r *http.Request) {
-
+func getUserData(w http.ResponseWriter, r *http.Request) User {
 	//To allocate slice for request body
 	length, err := strconv.Atoi(r.Header.Get("Content-Length"))
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
-		return
+		// return
 	}
 
 	//Read body data to parse json
@@ -26,15 +32,59 @@ func createUser(w http.ResponseWriter, r *http.Request) {
 	length, err = r.Body.Read(body)
 	if err != nil && err != io.EOF {
 		w.WriteHeader(http.StatusInternalServerError)
-		return
+		// return
 	}
 
 	//parse json
-	var jsonBody map[string]interface{}
-	err = json.Unmarshal(body[:length], &jsonBody)
+	var user User
+	err = json.Unmarshal(body[:length], &user)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
-		return
+		// return
 	}
-	fmt.Printf("%v\n", jsonBody["name"])
+	return user
+}
+
+func insertData(user User) {
+	db, err := sql.Open("mysql", "root:@/techtrain-mission-gameapi")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
+
+	ins, err := db.Prepare("INSERT INTO user(name, token) VALUES(?,?)")
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = ins.Exec(user.Name, user.Token)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("inserted", user.Name, user.Token)
+}
+
+func createUser(w http.ResponseWriter, r *http.Request) {
+	user := getUserData(w, r)
+
+	// 鍵となる文字列(多分なんでもいい)
+	secret := "secret"
+
+	// Token を作成
+	// jwt -> JSON Web Token - JSON をセキュアにやり取りするための仕様
+	// jwtの構造 -> {Base64 encoded Header}.{Base64 encoded Payload}.{Signature}
+	// HS254 -> 証明生成用(https://ja.wikipedia.org/wiki/JSON_Web_Token)
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"name": user.Name,
+		"iss":  "__init__", // JWT の発行者が入る(文字列(__init__)は任意)
+	})
+
+	//Dumpを吐く
+	// spew.Dump(token)
+
+	tokenString, _ := token.SignedString([]byte(secret))
+	user.Token = tokenString
+
+	insertData(user)
+	fmt.Print(user)
+
 }
